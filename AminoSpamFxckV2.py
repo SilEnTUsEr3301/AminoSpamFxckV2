114977310be35408e7b7e0f706c7c9c2c266428b
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-09-02 20:26:30 +0600

    Create AminoSpamFxckV2.py

---
+++ AminoSpamFxckV2.py
@@ -0,0 +1,27 @@
+import AminoLab
+import pyfiglet
+import concurrent.futures
+from colored import fore, back, style, attr
+attr(0)
+print(fore.DARK_GREEN_SEA + style.BOLD)
+print("""Script by Lil Zevi
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("Aminospamfxckv2", font="eftitalic"))
+client = AminoLab.Client()
+email = input("Email >> ")
+password = input("Password >> ")
+client.auth(email=email, password=password)
+msg = input("Message >> ")
+msgtype = input("Message Type >> ")
+clients = client.my_communities()
+for x, name in enumerate(clients.name, 1):
+	print(f"{x}.{name}")
+ndcId = clients.ndcId[int(input("Select the community >> "))-1]
+chats = client.my_chat_threads(ndcId=ndcId, size=100)
+for z, title in enumerate(chats.title, 1):
+	print(f"{z}.{title}")
+threadId = chats.threadId[int(input("Select The Chat >> "))-1]
+while True:
+	print("Spamming...")
+	with concurrent.futures.ThreadPoolExecutor(max_workers=150) as executor:
+		_ = [executor.submit(client.send_message, ndcId, threadId, msg, msgtype) for _ in range(100)]
